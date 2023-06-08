115e2822af52ff70db79bde9fdf945fea9b44fda
Павел Князев <Kardif86@rambler.ru>
2023-06-08 12:28:06 +0300

    4

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-        'HOST': os.getenv('DB_HOST', default='158.160.52.108'),
+        'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
